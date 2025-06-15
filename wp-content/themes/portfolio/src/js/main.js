c6dc3fb37a961ce0548a090ae29641529790a92a
Aleksander Onysiak <114723280+Aleksander-Onysiak@users.noreply.github.com>
2025-06-15 13:37:34 +0200

    build new files and fix form

--- wp-content/themes/portfolio/src/js/main.js
+++ wp-content/themes/portfolio/src/js/main.js
@@ -1,23 +1,4 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const spiralItems = document.querySelectorAll('.container.spiral > *');
-
-  spiralItems.forEach(item => {
-    if (item instanceof HTMLElement) {
-      item.addEventListener('click', () => {
-        // Enlever la classe highlight de tous
-        spiralItems.forEach(i => {
-          if (i instanceof HTMLElement) {
-            i.classList.remove('highlight');
-          }
-        });
-        // Ajouter highlight sur l'élément cliqué
-        item.classList.add('highlight');
-      });
-    }
-  });
-});
-
-document.addEventListener('DOMContentLoaded', () => {
   const galleries = document.querySelectorAll('.gallery_container');
 
   galleries.forEach(gallery => {
